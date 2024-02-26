d97016353696a9eeb9f678d9d2457fadfec23cf0
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-25 18:09:10 -0600

    creating token when find user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,12 @@
 const User = require('../models/user.model.js');
+const Token = require('../models/token.model.js');
+const crypto = require('crypto');
 
 
 exports.userExist = (req, res) => {
   const { username, password } = req.body;
+
+
   User.findByCredentials(username, password, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
@@ -10,6 +14,17 @@ exports.userExist = (req, res) => {
       } else {
         return res.send({ message: 'No', code: -1, token: null });
       }
-    } else res.send(data);
+    } else {
+      if (data == 3) {
+        const token_val = crypto.randomBytes(33).toString('hex');
+        User.getUserByCredentials(username, password, (err, data) => {
+          Token.createToken(token_val, data.usr_id)
+        });
+        res.send({ token_val, data });
+      }
+      else {
+        res.send({ token_val: '', data });
+      }
+    }
   })
 }
