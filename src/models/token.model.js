29f18cba479abc4accee5d7a43838d2337e7312a
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 13:28:11 -0600

    deleting token

--- src/models/token.model.js
+++ src/models/token.model.js
@@ -12,7 +12,19 @@ Token.createToken = (token_val, id_usr) => {
   var _query = `insert into tbl_session_token(stk_value,stk_id_user)values('${token_val}','${id_usr}')`;
   sql.query(_query, (err, result) => {
     console.log(result);
-  });
+  })
+}
+
+Token.deleteToken = (id_usr, result) => {
+  var _query = `delete from tbl_session_token where stk_id_user='${id_usr}'`;
+  sql.query(_query, (err, res) => {
+    if (err) {
+      result(err, null);
+    }
+    else {
+      result(null, res);
+    }
+  })
 }
 
 module.exports = Token;
