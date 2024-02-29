631ad77b3c92860522a3441f5bab4d9c201dd944
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 23:34:28 -0600

    saving image on activity model

--- src/models/activity.model.js
+++ src/models/activity.model.js
@@ -58,5 +58,17 @@ Activity.createActivity = (name, description, date, result) => {
   });
 }
 
+Activity.saveImage = (idActivity, img, result) => {
+  var _query = `insert into tbl_act_images(img_id_act, img_src)values('${idActivity}', '${img}')`;
+  sql.query(_query, (err, data) => {
+    if (err) {
+      result(err, null);
+    }
+    else {
+      result(null, data)
+    }
+  });
+}
+
 module.exports = Activity;
 
