3e712b4636b231b973471ea59be8f9d1422f8cc3
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-03-07 23:23:33 -0600

    fixing multer

--- src/routes/files/index.js
+++ src/routes/files/index.js
@@ -2,10 +2,9 @@ const multer = require('multer');
 var express = require('express');
 var router = express.Router()
 const File = require('../../controllers/files.controller.js');
+const os = require('os');
 
-const upload = multer({ dest: 'uploads/' });
-
-
+const upload = multer({ dest: os.tmpdir() });
 router.post('/upload', upload.single('pdfFile'), File.upload);
 router.get('/pdfs', File.getAllFiles);
 
