6a41201e76970701545af06912b815685813a8a3
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-03-07 23:10:10 -0600

    vercel config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 3000
 const cors = require('cors')
-const sql = require('./models/db.js');
+const sql = require('./src/models/db.js');
 const fs = require('fs');
 
 
@@ -16,13 +16,16 @@ app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.json());
 app.use(cors());
-app.use('/auth', require('./routes/auth/index.js'))
-app.use('/activities', require('./routes/activities/index.js'))
-app.use('/class', require('./routes/classes/index.js'))
-app.use('/', require('./routes/files/index.js'))
+app.use('/auth', require('./src/routes/auth/index.js'))
+app.use('/activities', require('./src/routes/activities/index.js'))
+app.use('/class', require('./src/routes/classes/index.js'))
+app.use('/', require('./src/routes/files/index.js'))
 
 
 app.use(cors({ origin: 'http://localhost:5173' }))
+app.get('', (req, res) => {
+  res.send({ message: 'Testing working' });
+})
 
 
 
