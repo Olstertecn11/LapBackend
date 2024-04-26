b82640bf21499702535b2af8f27e68bbd955d52b
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-04-26 13:49:39 -0600

    updating databsae

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,13 @@
-module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '1234',
-  DB: 'bd_progreso'
-}
 // module.exports = {
-//   HOST: 'sql5.freesqldatabase.com',
-//   USER: 'sql5697981',
-//   PASSWORD: 'CEKW3pM4i2',
-//   PORT: '3306',
-//   DB: 'sql5697981'
+//   HOST: 'localhost',
+//   USER: 'root',
+//   PASSWORD: '1234',
+//   DB: 'bd_progreso'
 // }
+module.exports = {
+  HOST: 'sql5.freesqldatabase.com',
+  USER: 'sql5697981',
+  PASSWORD: 'CEKW3pM4i2',
+  PORT: '3306',
+  DB: 'sql5697981'
+}
